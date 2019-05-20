c4966b3087327f4892f79a6e2485a78bdb338cdb
2662729661@qq.com <2662729661@qq.com>
2019-05-20 17:30:00 +0800

    修正

--- src/main/java/com/oclye/controller/ChatController.java
+++ src/main/java/com/oclye/controller/ChatController.java
@@ -70,7 +70,11 @@ public class ChatController {
 		String content = date + "【" + userid + "】对你说：" + ctx;
 		String contents = date + " 你对【" + touser + "】说：" + ctx;
 
-		template.convertAndSendToUser(userid, "/topic/private", new ChatMessage(userid, contents, touser, date));
+		if (userid.equals("客服小姐")) {
+			template.convertAndSendToUser(userid, "/topic/private", new ChatMessage(touser, contents, touser, date));
+		} else {
+			template.convertAndSendToUser(userid, "/topic/private", new ChatMessage(userid, contents, touser, date));
+		}
 		Thread.sleep(1000);
 		if ("阳仔".equals(touser)) {
 			touser = userid;
